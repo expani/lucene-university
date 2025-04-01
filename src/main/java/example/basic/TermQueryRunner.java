3d1647f610b26338364c2d6b7356a6c073ae23fe
expani <anijainc@amazon.com>
2025-04-01 04:06:50 -0700

    Added time elapsed
    Signed-off-by: expani <anijainc@amazon.com>

--- src/main/java/example/basic/TermQueryRunner.java
+++ src/main/java/example/basic/TermQueryRunner.java
@@ -15,7 +15,7 @@ import java.nio.file.Paths;
 public class TermQueryRunner {
 
     public static void main(String[] args) throws IOException {
-        
+
         if (args.length != 3) {
             System.out.println("Usage: <FieldName> <Field Value> <index dir>");
             System.exit(1);
@@ -24,17 +24,19 @@ public class TermQueryRunner {
         String fieldName = args[0];
         String fieldValue = args[1];
         String indexPath = args[2];
-        
+
         try (IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)))) {
             IndexSearcher searcher = new IndexSearcher(reader);
-            TopDocs topDocs = searcher.search(new TermQuery(new Term(fieldName, fieldValue)), 100);
+            long start = System.nanoTime();
+            TopDocs topDocs = searcher.search(new TermQuery(new Term(fieldName, fieldValue)), 10_000);
+            double took = (System.nanoTime() - start) / 1_000_000.0;
             int maxDocId = Integer.MIN_VALUE;
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
                 maxDocId = Math.max(maxDocId, scoreDoc.doc);
             }
-            System.out.println("Query completed with max doc id: " + maxDocId);
+            System.out.println("Query completed with max doc id: " + maxDocId + ", took: " + took + " milliseconds");
         }
-        
+
     }
-    
+
 }
